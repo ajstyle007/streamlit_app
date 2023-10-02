6c1fcdc99861aa9cb9d1e1ca93053d4fe9d60f08
ajay <musk.paradox.0@gmail.com>
2023-10-02 18:14:17 +0530

    update age slider

--- age_slider.py
+++ age_slider.py
@@ -3,12 +3,24 @@ import pandas as pd
 
 st.title(":blue[BMI INDEX CALCULATOR.]")
 st.markdown("#### :violet[We need few details to calulate your BMI.]")
-
-weight = st.slider("How much is your weight?", 0,150,70)
+ 
+weight = st.slider("How much is your weight?", 0.0,150.0,70.0)
 st.write("Your weight is ",weight,"Kg.")
 
-height = st.slider("How much is  your Height in feet?", 4.0, 8.0, 5.0)
-st.write("Your height is ", height,"feet.")
+feet1, inch1 = st.columns(2)
+
+with feet1:
+
+    height_feet = st.slider("How much is your Height in feet?", 4.00, 8.00, 5.00)
+    
+    
+with inch1:
+     
+    inch = st.slider("How many inches?", 0.0,12.0,2.0)
+    inch_into_feet = inch/12
+ 
+height =  inch_into_feet+height_feet
+st.write("Your height is ", round(height_feet,3),"feet", inch, "inches.")
 
 #convert feet into meter
 h_meter = height*0.3048 #or divide by 3.281
@@ -30,7 +42,7 @@ with col1:
     elif r_bmi>25 and r_bmi<29.9:
             st.markdown("### :red[Your are Over weight.]")
     elif r_bmi>30:
-            st.markdown("### :rainbow[Your are Obeses.]")
+            st.markdown("### :rainbow[Your are Obese.]")
 with col2:
     dict_bmi = {
         "Weight Catogory": ["Underweight","Normal weight","Overweight","Obesity"],
